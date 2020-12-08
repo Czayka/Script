41280daaafcc1917820a6c25a9adf8d128e95776
Czayka <40207746+Czayka@users.noreply.github.com>
2020-12-08 23:56:29 +0800

    Update shareCode-auto.js

--- shareCode-auto.js
+++ shareCode-auto.js
@@ -91,7 +91,7 @@ function checkWhetherNeedAgain(resp, fun, url, name) {
 function showMsg() {
   return new Promise((resolve) => {
     $.msg($.name, "", $.result.join("\n"));
-    notify.sendNotify($.msg,$.name, "", $.result.join("\n"));
+    notify.sendNotify("互助码",$.msg);
     resolve();
   });
 }

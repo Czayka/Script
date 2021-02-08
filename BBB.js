2c0a4d5f06873654447ae2445806a855e62d0c76
Czayka <40207746+Czayka@users.noreply.github.com>
2021-02-08 22:06:34 +0800

    Update BBB.js

--- BBB.js
+++ BBB.js
@@ -79,8 +79,6 @@ function showCookie(){
 
 function showmsg(){
       $.msg($.name, '', notice)
-      
-            notify.sendNotify("步步宝",($.name, '', notice));
       if((acTimeHour === 12 || acTimeHour ===23) && (acTimeMinutes >= 0 || acTimeMinutes <= 30)){
             notify.sendNotify("步步宝",($.name, '', notice));
       }

3d81d953cd86ee63174c90875b1123eb80301938
Czayka <40207746+Czayka@users.noreply.github.com>
2021-05-19 00:57:00 +0800

    Update Zy.js

--- Zy.js
+++ Zy.js
@@ -1,9 +1,6 @@
 var modifiedHeaders = $request.headers;
 
-var modifiedBodys = $request.body;
 
 modifiedHeaders['device-id'] = '1636b97d05b453db65b451aa1424bab2';
-
-modifiedBodys['promo_code'] = '1'
 
 $done({headers : modifiedHeaders});

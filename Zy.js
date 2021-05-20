09e21a6f0d738c114c5effd65c57d1708c633e05
Czayka <40207746+Czayka@users.noreply.github.com>
2021-05-21 00:20:48 +0800

    Update Zy.js

--- Zy.js
+++ Zy.js
@@ -1,6 +1,6 @@
 var modifiedHeaders = $request.headers;
 
 
-modifiedHeaders['device-id'] = '1636b97d05b453yb65b451aa1424bab2';
+modifiedHeaders['device-id'] = '1636c97d05b453yb65b451aa1424bab2';
 
 $done({headers : modifiedHeaders});
